dc787d0a6ae5bad6577777f6ad9446531b23ab24
Alan Nosenkis <nalanpa@yandex.ru>
2017-06-09 06:30:37 +0300

    png to gif

--- client/index.js
+++ client/index.js
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './app.css';
-import './icons/user.png';
-import './icons/login.png';
-import './icons/logout.png';
-import './icons/signup.png';
-import './icons/users.png';
-import './icons/splash_screen.png';
+import './icons/user.gif';
+import './icons/login.gif';
+import './icons/logout.gif';
+import './icons/signup.gif';
+import './icons/users.gif';
+import './icons/splash_screen.gif';
